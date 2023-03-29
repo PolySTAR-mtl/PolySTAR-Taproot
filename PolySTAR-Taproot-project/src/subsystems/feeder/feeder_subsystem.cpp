43de9b0a9513a64d18f00e08c51054a2a0f55207
Fadi <beirouty.fadi@gmail.com>
2023-03-29 18:40:41 -0400

    style: restyling feeder_subsystem

--- PolySTAR-Taproot-project/src/subsystems/feeder/feeder_subsystem.cpp
+++ PolySTAR-Taproot-project/src/subsystems/feeder/feeder_subsystem.cpp
@@ -1,5 +1,4 @@
 #include "feeder_subsystem.hpp"
-
 #include "tap/communication/serial/remote.hpp"
 #include "tap/algorithms/math_user_utils.hpp"
 #include "control/drivers/drivers.hpp"
@@ -7,12 +6,11 @@
 using namespace tap;
 using tap::communication::serial::Uart;
 
-namespace control
-{
-namespace feeder
-{
-void FeederSubsystem::initialize()
-{
+namespace control {
+
+namespace feeder {
+
+void FeederSubsystem::initialize(){
     feederMotor.initialize();
 }
 
@@ -24,19 +22,18 @@ void FeederSubsystem::updateRpmPid(modm::Pid<float>* pid, tap::motor::DjiMotor* 
     int16_t shaftRPM = motor->getShaftRPM();
     if (desiredRPM == 0) {
         motor->setDesiredOutput(0);
-    } else {
+    } 
+    else {
         pid->update(desiredRPM - shaftRPM);
         float pidValue = pid->getValue();
         motor->setDesiredOutput(pidValue);
     }
-
 }
 
 /*
     Give desired setpoints for feeder movement.
 */
-void FeederSubsystem::setDesiredOutput(float rpm) 
-{
+void FeederSubsystem::setDesiredOutput(float rpm) {
     feederDesiredRpm = rpm;
 }
 
--- PolySTAR-Taproot-project/src/subsystems/feeder/feeder_subsystem.hpp
+++ PolySTAR-Taproot-project/src/subsystems/feeder/feeder_subsystem.hpp
@@ -7,15 +7,15 @@
 #include "tap/util_macros.hpp"
 #include "feeder_constants.hpp"
 
-namespace control
-{
-namespace feeder
-{
+namespace control {
+
+namespace feeder {
+
 /**
  * A bare bones Subsystem for interacting with a feeder.
  */
-class FeederSubsystem : public tap::control::Subsystem
-{
+class FeederSubsystem : public tap::control::Subsystem {
+
 public:
 
     /**
@@ -26,14 +26,12 @@ public:
         : tap::control::Subsystem(drivers),
           feederMotor(drivers, FEEDER_MOTOR_ID, CAN_BUS_MOTORS, false, "feeder motor"),
           feederPid(FEEDER_PID_KP,FEEDER_PID_KI,FEEDER_PID_KD,FEEDER_PID_MAX_ERROR_SUM,FEEDER_PID_MAX_OUTPUT)
-
-    {
-    }
+    {}
 
     FeederSubsystem(const FeederSubsystem &other) = delete;
 
     FeederSubsystem &operator=(const FeederSubsystem &other) = delete;
-
+    
     ~FeederSubsystem() = default;
 
     void initialize() override;
@@ -47,23 +45,23 @@ public:
     const tap::motor::DjiMotor &getFeederMotor() const { return feederMotor; }
 
 private:
-    ///< Hardware constants, not specific to any particular feeder.
+    // Hardware constants, not specific to any particular feeder.
     static constexpr tap::motor::MotorId FEEDER_MOTOR_ID = tap::motor::MOTOR8;
     static constexpr tap::can::CanBus CAN_BUS_MOTORS = tap::can::CanBus::CAN_BUS1;
 
-    ///< Motors.  Use these to interact with any dji style motors.
+    // Motors.  Use these to interact with any dji style motors.
     tap::motor::DjiMotor feederMotor;
 
     // PID controllers for position feedback from motors
     modm::Pid<float> feederPid;
 
-    /// Activating the command sets a desired RPM (defined in feeder_constants.hpp) for the motor.
+    // Activating the command sets a desired RPM (defined in feeder_constants.hpp) for the motor.
     float feederDesiredRpm;
+    
+}; // class FeederSubsystem
 
-};  // class FeederSubsystem
+} // namespace feeder
 
-}  // namespace feeder
-
-}  // namespace control
+} // namespace control
 
 #endif  // FEEDER_SUBSYSTEM_HPP_
