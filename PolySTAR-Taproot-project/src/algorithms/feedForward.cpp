05aa53b8549a4cc846f98cf4c342841a58e12f5b
Olivier Frechette <olivier.frechette@polymtl.ca>
2023-04-03 13:44:22 -0400

    Implement FeedForward Controller

---
+++ PolySTAR-Taproot-project/src/algorithms/feedForward.cpp
@@ -0,0 +1,28 @@
+#include "feedforward.hpp"
+
+#include "tap/algorithms/math_user_utils.hpp"
+
+using namespace tap::algorithms;
+
+namespace src
+{
+namespace algorithms
+{
+FeedForward::FeedForward(const FeedForwardConfig &pidConfig)
+    : config(pidConfig)
+{
+}
+
+float FeedForward::calculate(float velocity) {
+    velocity = tap::algorithms::limitVal<float>(velocity, -config.maxVelocity, config.maxVelocity);
+    return config.ks * tap::algorithms::getSign(velocity) + config.kv * velocity;
+}
+
+float FeedForward::calculateWithGravity(float velocity, float angle) {
+    velocity = tap::algorithms::limitVal<float>(velocity, -config.maxVelocity, config.maxVelocity);
+    return config.ks * tap::algorithms::getSign(velocity) + config.kv * velocity + config.kg * cosf(angle*DEG_TO_RAD);
+}
+
+}  // namespace algorithms
+
+}  // namespace src
