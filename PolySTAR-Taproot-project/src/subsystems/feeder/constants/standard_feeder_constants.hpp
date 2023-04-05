141e75764ad18d3074d4012f9bd5db18f3a9070b
Fadi <beirouty.fadi@gmail.com>
2023-04-05 16:51:30 -0400

    chore: creating seperate constant files for feeder for each robot

---
+++ PolySTAR-Taproot-project/src/subsystems/feeder/constants/standard_feeder_constants.hpp
@@ -0,0 +1,28 @@
+/**
+ * Feeder subsystem constants for standard
+ */
+
+/**
+ * Feeder RPM PID: A PID controller for feeder RPM. The PID parameters for the
+ * controller are listed below.
+ */
+static constexpr float FEEDER_PID_KP = 20.0f;
+static constexpr float FEEDER_PID_KI = 5.0f;
+static constexpr float FEEDER_PID_KD = 0.0f;
+static constexpr float FEEDER_PID_MAX_ERROR_SUM = 5000.0f;
+static constexpr float FEEDER_PID_MAX_OUTPUT = 8000.0f;
+
+
+/**
+ * The feeder RPM set when the feeder is on
+*/
+static constexpr float FEEDER_RPM = 2500;
+static constexpr float FEEDER_REVERSE_RPM = -1500;
+
+
+/*
+ * Feedermotor rotation direction:
+ * True = clockwise
+ * False = anticlockwise
+*/
+static constexpr bool ROTATION_DIRECTION = true;
