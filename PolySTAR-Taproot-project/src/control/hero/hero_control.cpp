8ca57c76c1b91466b0d13fbd8d931d9c82b77729
Melvice Guimfack <melvice-junior.guimfack@polymtl.ca>
2023-05-06 15:49:42 -0400

    hero control and constant

--- PolySTAR-Taproot-project/src/control/hero/hero_control.cpp
+++ PolySTAR-Taproot-project/src/control/hero/hero_control.cpp
@@ -1,7 +1,10 @@
 #ifdef TARGET_HERO
 
+#ifdef TARGET_STANDARD
+
 #include "tap/control/command_mapper.hpp"
 #include "tap/control/hold_command_mapping.hpp"
+#include "tap/control/hold_repeat_command_mapping.hpp"
 #include "tap/control/toggle_command_mapping.hpp"
 #include "control/drivers/drivers_singleton.hpp"
 #include "control/drivers/drivers.hpp"
@@ -16,25 +19,27 @@
 // Turret includes
 #include "subsystems/turret/turret_subsystem.hpp"
 #include "subsystems/turret/turret_manual_aim_command.hpp"
-// #include "subsystems/turret/turret_left_aim_command.hpp"
-// #include "subsystems/turret/turret_right_aim_command.hpp"
+#include "subsystems/turret/turret_left_aim_command.hpp"
+#include "subsystems/turret/turret_right_aim_command.hpp"
 #include "subsystems/turret/turret_mouse_aim_command.hpp"
 
 // Feeder includes
 #include "subsystems/feeder/feeder_subsystem.hpp"
-#include "subsystems/feeder/feeder_feed_command.hpp"
+#include "subsystems/feeder/feeder_move_unjam_command.hpp"
 #include "subsystems/feeder/feeder_reverse_command.hpp"
+#include "subsystems/feeder/feeder_move_command.hpp"
 
 //Flywheel includes
 #include "subsystems/flywheel/flywheel_subsystem.hpp"
 #include "subsystems/flywheel/flywheel_fire_command.hpp"
-#include "subsystems/flywheel/fire_command_group.hpp"
+// #include "subsystems/flywheel/fire_command_group.hpp"
 
 using src::DoNotUse_getDrivers;
 using src::control::RemoteSafeDisconnectFunction;
 using tap::communication::serial::Remote;
 using tap::control::CommandMapper;
 using tap::control::HoldCommandMapping;
+using tap::control::HoldRepeatCommandMapping;
 using tap::control::ToggleCommandMapping;
 using tap::control::RemoteMapState;
 
@@ -51,44 +56,38 @@ namespace control
 /* define subsystems --------------------------------------------------------*/
 chassis::ChassisSubsystem theChassis(drivers());
 turret::TurretSubsystem theTurret(drivers());
-feeder::FeederSubsystem theFeeder(drivers());
+feeder::FeederSubsystem topFeeder(drivers());
 flywheel::FlywheelSubsystem theFlywheel(drivers());
 
 /* define commands ----------------------------------------------------------*/
 chassis::ChassisDriveCommand chassisDrive(&theChassis, drivers());
 chassis::ChassisKeyboardDriveCommand chassisKeyboardDrive(&theChassis, drivers());
 chassis::ChassisCalibrateImuCommand chassisImuCalibrate(&theChassis, drivers());
+
 turret::TurretManualAimCommand turretManualAim(&theTurret, drivers());
-// turret::TurretLeftAimCommand turretLeftAim(&theTurret, drivers());
-// turret::TurretRightAimCommand turretRightAim(&theTurret, drivers());
+turret::TurretLeftAimCommand turretLeftAim(&theTurret, drivers());
+turret::TurretRightAimCommand turretRightAim(&theTurret, drivers());
 turret::TurretMouseAimCommand turretMouseAim(&theTurret, drivers());
-feeder::FeederFeedCommand feederForward(&theFeeder, drivers());
-feeder::FeederReverseCommand feederReverse(&theFeeder, drivers());
+
+feeder::FeederMoveUnjamCommand feederMoveUnjam(&topFeeder, drivers());
+feeder::FeederMoveCommand feederMove(&topFeeder);
+
 flywheel::FlywheelFireCommand flywheelStart(&theFlywheel, drivers());
-flywheel::FireCommandGroup fireCommandGroup(&theFlywheel, &theFeeder, drivers());
-flywheel::FireEndCommandGroup fireEndCommandGroup(&theFlywheel, &theFeeder, drivers());
 
 /* safe disconnect function -------------------------------------------------*/
 RemoteSafeDisconnectFunction remoteSafeDisconnectFunction(drivers());
 
 /* define command mappings --------------------------------------------------*/
-// HoldCommandMapping feedFeeder(drivers(), {&feederForward}, RemoteMapState(Remote::Switch::RIGHT_SWITCH, Remote::SwitchState::UP));
-// HoldCommandMapping flywheelFire(drivers(), {&flywheelStart}, RemoteMapState(Remote::Switch::LEFT_SWITCH, Remote::SwitchState::UP));
-// HoldCommandMapping rightAimTurret(drivers(), {&turretRightAim}, RemoteMapState(Remote::Switch::LEFT_SWITCH, Remote::SwitchState::UP));
-// HoldCommandMapping leftAimTurret(drivers(), {&turretLeftAim}, RemoteMapState(Remote::Switch::LEFT_SWITCH, Remote::SwitchState::DOWN));
-HoldCommandMapping reverseFeeder(drivers(), {&feederReverse}, RemoteMapState(Remote::Switch::RIGHT_SWITCH, Remote::SwitchState::DOWN));
-HoldCommandMapping remoteFireCommandGroup(drivers(), {&fireCommandGroup}, RemoteMapState(Remote::Switch::RIGHT_SWITCH, Remote::SwitchState::UP));
-HoldCommandMapping mouseFireCommandGroup(drivers(), {&fireCommandGroup}, RemoteMapState(RemoteMapState::MouseButton::LEFT));
+HoldRepeatCommandMapping feedFeeder(drivers(), {&feederMoveUnjam}, RemoteMapState(Remote::Switch::RIGHT_SWITCH, Remote::SwitchState::UP),true);
+HoldCommandMapping startFlywheel(drivers(), {&flywheelStart}, RemoteMapState(Remote::Switch::LEFT_SWITCH, Remote::SwitchState::UP));
 ToggleCommandMapping toggleChassisDrive(drivers(), {&chassisKeyboardDrive}, RemoteMapState({Remote::Key::G}));
 ToggleCommandMapping turretMouseAimToggle(drivers(), {&turretMouseAim}, RemoteMapState({Remote::Key::B}));
-
-// HoldCommandMapping stopFiring(drivers(), {&fireEndCommandGroup}, RemoteMapState(Remote::Switch::RIGHT_SWITCH, Remote::SwitchState::MID));
 
 /* register subsystems here -------------------------------------------------*/
 void registerStandardSubsystems(src::Drivers *drivers) {
     drivers->commandScheduler.registerSubsystem(&theChassis);
     drivers->commandScheduler.registerSubsystem(&theTurret);
-    drivers->commandScheduler.registerSubsystem(&theFeeder);
+    drivers->commandScheduler.registerSubsystem(&topFeeder);
     drivers->commandScheduler.registerSubsystem(&theFlywheel);
 }
 
@@ -96,7 +95,7 @@ void registerStandardSubsystems(src::Drivers *drivers) {
 void initializeSubsystems() {
     theChassis.initialize();
     theTurret.initialize();
-    theFeeder.initialize();
+    topFeeder.initialize();
     theFlywheel.initialize();
 }
 
@@ -104,7 +103,7 @@ void initializeSubsystems() {
 void setDefaultStandardCommands(src::Drivers *) {
     theChassis.setDefaultCommand(&chassisDrive);
     theTurret.setDefaultCommand(&turretManualAim);
-    theFeeder.setDefaultCommand(&fireEndCommandGroup);
+    // theFeeder.setDefaultCommand(&fireEndCommandGroup);
 }
 
 /* add any starting commands to the scheduler here --------------------------*/
@@ -114,10 +113,8 @@ void startStandardCommands(src::Drivers *drivers) {
 
 /* register io mappings here ------------------------------------------------*/
 void registerStandardIoMappings(src::Drivers *drivers) {  
-    drivers->commandMapper.addMap(&remoteFireCommandGroup);
-    drivers->commandMapper.addMap(&mouseFireCommandGroup);
-    // drivers->commandMapper.addMap(&stopFiring);
-    drivers->commandMapper.addMap(&reverseFeeder);
+    drivers->commandMapper.addMap(&feedFeeder);
+    drivers->commandMapper.addMap(&startFlywheel);
 //    drivers->commandMapper.addMap(&leftAimTurret);
 //    drivers->commandMapper.addMap(&rightAimTurret);
     drivers->commandMapper.addMap(&toggleChassisDrive);
@@ -133,7 +130,7 @@ void initSubsystemCommands(src::Drivers *drivers)
     startStandardCommands(drivers);
     registerStandardIoMappings(drivers);
     char buffer[50];
-    int nBytes = sprintf(buffer,"Initializing Hero\n");
+    int nBytes = sprintf(buffer,"Initializing Standard\n");
     drivers->uart.write(tap::communication::serial::Uart::UartPort::Uart6,(uint8_t*) buffer, nBytes+1);
 }
 
--- PolySTAR-Taproot-project/src/subsystems/feeder/constants/hero_feeder_constants.hpp
+++ PolySTAR-Taproot-project/src/subsystems/feeder/constants/hero_feeder_constants.hpp
@@ -7,9 +7,9 @@
  */
 
 static constexpr tap::algorithms::SmoothPidConfig FEEDER_PID_CONFIG(
-    0.0f, // kP
+    0.075f, // kP
     0.0f, // kI
-    0.0f, // kD
+    -7.5f, // kD
     5000.0f, // Max error sum
     16000.0f, // Max output
     1.0f, // TQ Derivative Kalman
@@ -26,7 +26,7 @@ static constexpr tap::algorithms::SmoothPidConfig FEEDER_PID_CONFIG(
  */
 
 static constexpr src::algorithms::FeedForwardConfig FEEDER_FF_CONFIG(
-    1200.0f, // kS
+    400.0f, // kS
     0.0f, // kV
     0.0f, // kG
     1000.0f // maxVelocity
@@ -39,13 +39,15 @@ static constexpr float FEEDER_RPM = 2500;
 static constexpr float FEEDER_REVERSE_RPM = -1500;
 
 static constexpr float DEGREE_TO_TICK = 8192*36/360; // 8192 Ticks per turn, 36:1 gear ratio 
-static constexpr float UNJAM_MAX_WAIT_TIME_MS = 2000; // TO BE DETERMINED
-static constexpr float MOVE_DISPLACEMENT_TICK = 45*DEGREE_TO_TICK; // TO BE DETERMINED
+static constexpr float UNJAM_MAX_WAIT_TIME_MS = 500; // TO BE DETERMINED
+static constexpr float MOVE_DISPLACEMENT_TICK = 90*DEGREE_TO_TICK; // TO BE DETERMINED 
 static constexpr float UNJAM_CYCLES = 4; // TO BE DETERMINED
 static constexpr float UNJAM_DISPLACEMENT_TICK = 45*DEGREE_TO_TICK; // TO BE DETERMINED
-static constexpr float PAUSE_AFTER_MOVE_TIME_MS = 500; // TO BE DETERMINED
-static constexpr float MOVE_TIME_MS = 125; // TO BE DETERMINED
+static constexpr float PAUSE_AFTER_MOVE_TIME_MS = 100; // TO BE DETERMINED ////////
+static constexpr float MOVE_TIME_MS = 125; // TO BE DETERMINED //////
 static constexpr float SETPOINT_POS_TOLERANCE_TICK = 1*DEGREE_TO_TICK; // TO BE DETERMINED
 
 static constexpr float JAM_CHECKER_TOLERANCE_TICK = 5*DEGREE_TO_TICK;
-static constexpr uint32_t JAM_CHECKER_TOLERANCE_MS = 1000;
+static constexpr uint32_t JAM_CHECKER_TOLERANCE_MS = 500;
+
+static constexpr bool IS_FEEDER_INVERTED = false;
