8ca29e4ad6c995f7a455cf0d0dd6351e815e325d
IsabellaSantacruz <isabella.santacruz@polymtl.ca>
2023-11-10 16:30:52 -0500

    constructeur commencé

---
+++ PolySTAR-Taproot-project/src/subsystems/chassis/chassis_constants.hpp
@@ -0,0 +1,19 @@
+#ifndef CHASSIS_CONSTANTS_HPP_
+#define CHASSIS_CONSTANTS_HPP_
+
+/**
+ * Chassis PID: A PID controller for chassis motors. The PID parameters for the
+ * controller are listed below.
+ */
+ static constexpr float CHASSIS_PID_KP = 20.0f;
+ static constexpr float CHASSIS_PID_KI = 0.2f;
+ static constexpr float CHASSIS_PID_KD = 0.0f;
+ static constexpr float CHASSIS_PID_MAX_ERROR_SUM = 5000.0f;
+ static constexpr float CHASSIS_PID_MAX_OUTPUT = 16000.0f;
+
+ /**
+ * Inverted directions (????)
+ */
+static constexpr float CHASSIS_MOTOR_IS_INVERTED = false; // Pk float?
+
+#endif
--- PolySTAR-Taproot-project/src/subsystems/chassis/chassis_subsystem.hpp
+++ PolySTAR-Taproot-project/src/subsystems/chassis/chassis_subsystem.hpp
@@ -2,6 +2,7 @@
 #define CHASSIS_SUBSYSTEM_HPP_
 
 #include "tap/control/subsystem.hpp"
+#include "chassis_constants.hpp"
 
 namespace control
 {
@@ -11,8 +12,20 @@ class ChassisSubsystem : public tap::control::Subsystem
 {
 public:
 
+    /* Questions :
+    * Voir constants
+    */
     ChassisSubsystem(tap::Drivers *drivers)
-    : tap::control::Subsystem(drivers) {}
+    : tap::control::Subsystem(drivers),
+    chassisMotorBL(drivers, CHASSIS_MOTOR_ID_BL, CAN_BUS_MOTORS, CHASSIS_MOTOR_IS_INVERTED, "BL motor"),
+    chassisMotorBR(drivers, CHASSIS_MOTOR_ID_BR, CAN_BUS_MOTORS, CHASSIS_MOTOR_IS_INVERTED, "BR motor"),
+    chassisMotorFL(drivers, CHASSIS_MOTOR_ID_FL, CAN_BUS_MOTORS, CHASSIS_MOTOR_IS_INVERTED, "FL motor"),
+    chassisMotorFR(drivers, CHASSIS_MOTOR_ID_FR, CAN_BUS_MOTORS, CHASSIS_MOTOR_IS_INVERTED, "FR motor"),
+    BLController(CHASSIS_PID_KP, CHASSIS_PID_KI, CHASSIS_PID_KD, CHASSIS_PID_MAX_ERROR_SUM, CHASSIS_PID_MAX_OUTPUT),
+    BRController(CHASSIS_PID_KP, CHASSIS_PID_KI, CHASSIS_PID_KD, CHASSIS_PID_MAX_ERROR_SUM, CHASSIS_PID_MAX_OUTPUT),
+    FLController(CHASSIS_PID_KP, CHASSIS_PID_KI, CHASSIS_PID_KD, CHASSIS_PID_MAX_ERROR_SUM, CHASSIS_PID_MAX_OUTPUT),
+    FRController(CHASSIS_PID_KP, CHASSIS_PID_KI, CHASSIS_PID_KD, CHASSIS_PID_MAX_ERROR_SUM, CHASSIS_PID_MAX_OUTPUT),
+    {}
 
     ~ChassisSubsystem() = default;
 
@@ -27,12 +40,31 @@ public:
     void refresh() override;
 
 private:
+    ///< Hardware constants
+    // TODO change motor IDs to correct ones
+    static constexpr tap::motor::MotorId CHASSIS_MOTOR_ID_BL = tap::motor::MOTOR1;
+    static constexpr tap::motor::MotorId CHASSIS_MOTOR_ID_BR = tap::motor::MOTOR2;
+    static constexpr tap::motor::MotorId CHASSIS_MOTOR_ID_FL = tap::motor::MOTOR3;
+    static constexpr tap::motor::MotorId CHASSIS_MOTOR_ID_FR = tap::motor::MOTOR4;
+
+    static constexpr tap::can::CanBus CAN_BUS_MOTORS = tap::can::CanBus::CAN_BUS1;
+
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
-    static constexpr float CHASSIS_PID_KI = 0.2f;
-    static constexpr float CHASSIS_PID_KD = 0.0f;
-    static constexpr float CHASSIS_PID_MAX_ERROR_SUM = 5000.0f;
-    static constexpr float CHASSIS_PID_MAX_OUTPUT = 16000.0f;
+   /// Desired wheel output for each motor
+    //std::array<float, static_cast<uint8_t>(MotorId::NUM_MOTORS)> desiredOutput;
+
+    ///< MotorContollers
+    modm::Pid<float> BLController;
+    modm::Pid<float> BRController;
+    modm::Pid<float> FLController;
+    modm::Pid<float> FRController;
+
+    ///< Motors
+    tap::motor::DjiMotor chassisMotorBL;
+    tap::motor::DjiMotor chassisMotorBR;
+    tap::motor::DjiMotor chassisMotorFL;
+    tap::motor::DjiMotor chassisMotorFR;
 };
 
 } // namespace chassis
