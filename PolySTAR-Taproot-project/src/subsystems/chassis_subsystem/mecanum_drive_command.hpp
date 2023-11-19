cea67b8db9e76fea11002f4eac3088e8487bd529
Harry LAW-YEN <harry.lawyen@gmail.com>
2023-11-18 21:05:41 -0500

    Piste pour execute() de tank_drive_command.cpp

---
+++ PolySTAR-Taproot-project/src/subsystems/chassis_subsystem/mecanum_drive_command.hpp
@@ -0,0 +1,43 @@
+#include "tap/control/command.hpp"
+
+#include "control/drivers/drivers.hpp"
+#include "tap/algorithms/math_user_utils.hpp"
+#include "tap/errors/create_errors.hpp"
+
+namespace control {
+    namespace chassis {
+        class Mecanum : public tap::control::Subsystem {
+        public:
+            MyCommand()
+
+            ~MyCommand() = default;
+
+            /**
+             * Called once when the subsystem is added to the scheduler.
+             */
+            void initialize() override;
+
+            /**
+             * Returns the command name. Used by the CommandScheduler and for debugging purposes.
+             */
+            const char *getName() const { return "Command Name"; }
+
+            /**
+             * Will be called periodically whenever the CommandScheduler runs.
+             */
+            void execute() override;
+
+            /**
+             * Will be called once when IsFinished() returns true or the command is interrupted
+             */
+            void end(bool) override;
+
+            /**
+             * Called periodically whenever the CommandScheduler runs. If it returns true, the end() method is called and the
+             * command is removed from the CommandScheduler.
+             */
+            void isFinished() const override;
+
+        };
+    }
+}
---
+++ PolySTAR-Taproot-project/src/subsystems/chassis_subsystem/tank_drive_command.cpp
@@ -0,0 +1,16 @@
+#include "tank_drive_command.hpp"
+
+namespace control {
+    namespace chassis {
+
+        void TankDriveCommand::initialize() {
+            chassis->initialize();
+        }
+
+        void TankDriveCommand::execute() {
+            
+        }
+
+
+    }
+}
