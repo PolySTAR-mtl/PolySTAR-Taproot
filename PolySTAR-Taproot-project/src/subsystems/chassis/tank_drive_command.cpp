0bd7873dd63ec1adcb708ee6d106bc9b12d92cd8
IsabellaSantacruz <isabella.santacruz@polymtl.ca>
2023-11-22 12:04:50 -0500

    TankDriveCommand (?)

--- PolySTAR-Taproot-project/src/subsystems/chassis/tank_drive_command.cpp
+++ PolySTAR-Taproot-project/src/subsystems/chassis/tank_drive_command.cpp
@@ -8,16 +8,33 @@ void TankDriveCommand::initialize() {
 
 }
 
-void TankDriveCommand::execute() {
-
-}
-
-void TankDriveCommand::end(bool) {
-
-}
-
-bool isFinished() {
+void TankDriveCommand::execute()
+{
+    //Get right stick vertical --> getTurretYInput
+    float rigthMotorsDesiredRpm = controlInterface.getTurretYInput() * rpmScaleFactor;
     
+    //Get left stick vertical  --> getChassisYInput
+    float leftMotorsDesiredRpm = controlInterface.getChassisYInput() * rpmScaleFactor;
+
+    chassis->setDesiredOutputMotor(CHASSIS_MOTOR_ID_BL, leftMotorsDesiredRpm);
+    chassis->setDesiredOutputMotor(CHASSIS_MOTOR_ID_BR, rigthMotorsDesiredRpm);
+    chassis->setDesiredOutputMotor(CHASSIS_MOTOR_ID_FL, leftMotorsDesiredRpm);
+    chassis->setDesiredOutputMotor(CHASSIS_MOTOR_ID_FR, rigthMotorsDesiredRpm);
+
+    chassis->refresh();
+}
+
+void TankDriveCommand::end(bool interrupted) // Toujours interrompu (?)
+{
+    chassis->setDesiredOutputMotor(CHASSIS_MOTOR_ID_BL, 0.0f);
+    chassis->setDesiredOutputMotor(CHASSIS_MOTOR_ID_BR, 0.0f);
+    chassis->setDesiredOutputMotor(CHASSIS_MOTOR_ID_FL, 0.0f);
+    chassis->setDesiredOutputMotor(CHASSIS_MOTOR_ID_FR, 0.0f);
+}
+
+bool isFinished()
+{
+    return false;
 }
 
 } // namespace chassis
--- PolySTAR-Taproot-project/src/subsystems/chassis/tank_drive_command.hpp
+++ PolySTAR-Taproot-project/src/subsystems/chassis/tank_drive_command.hpp
@@ -2,6 +2,8 @@
 #define TANK_DRIVE_COMMAND_HPP_
 
 #include "tap/control/command.hpp"
+#include "chassis_subsystem.hpp"
+#include "control_interface.hpp"
 
 namespace control 
 {
@@ -12,19 +14,27 @@ class TankDriveCommand : public tap::control::Command
 {
 public:
 
-    TankDriveCommand();
+    TankDriveCommand(ChassisSubsystem* const chassis, Drivers* drivers)
+    : chassis(chassis), controlInterface(drivers)
+    {
+        if (chassis == nullptr)
+        {
+            return;
+        }
+        this->addSubsystemRequirement(dynamic_cast<tap::control::Subsystem*>(chassis));
+    }
 
     ~TankDriveCommand() = default;
 
     /**
      * Called once when the subsystem is added to the scheduler.
      */
-    void initialize() override;
+    void initialize() override {}
 
     /**
      * Returns the command name. Used by the CommandScheduler and for debugging purposes.
      */
-    const char *getName() const { return "Command Name"; }
+    const char *getName() const { return "Tank Drive Command"; }
 
     /**
      * Will be called periodically whenever the CommandScheduler runs.
@@ -34,13 +44,22 @@ public:
     /**
      * Will be called once when IsFinished() returns true or the command is interrupted
      */
-    void end(bool) override;
+    void end(bool interrupted) override;
 
     /**
      * Called periodically whenever the CommandScheduler runs. If it returns true, the end() method is called and the
      * command is removed from the CommandScheduler.
      */
     bool isFinished() const override;
+
+private:
+    ChassisSubsystem* chassis;
+    src::control::ControlInterface controlInterface;
+
+    static constexpr tap::motor::MotorId CHASSIS_MOTOR_ID_BL = tap::motor::MOTOR1;
+    static constexpr tap::motor::MotorId CHASSIS_MOTOR_ID_BR = tap::motor::MOTOR2;
+    static constexpr tap::motor::MotorId CHASSIS_MOTOR_ID_FL = tap::motor::MOTOR3;
+    static constexpr tap::motor::MotorId CHASSIS_MOTOR_ID_FR = tap::motor::MOTOR4;
 
 };
 
