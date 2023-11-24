8c0a983f2372bcf39c662b1031587082f5390daf
IsabellaSantacruz <isabella.santacruz@polymtl.ca>
2023-11-23 22:09:24 -0500

    mecanum Drive ?

--- PolySTAR-Taproot-project/src/subsystems/chassis/mecanum_drive_command.cpp
+++ PolySTAR-Taproot-project/src/subsystems/chassis/mecanum_drive_command.cpp
@@ -7,8 +7,23 @@ namespace chassis
 void MecanumDriveCommand::execute()
 {
     // Get remote values
+    float yDesiredOutput = controlInterface.getChassisYInput();
+    float yAbs = yDesiredOutput > 0 ? yDesiredOutput : -yDesiredOutput;
 
-    // Set motors desired output
+    float xDesiredOutput = controlInterface.getChassisXInput();
+    float xAbs = xDesiredOutput > 0 ? xDesiredOutput : -xDesiredOutput;
+     
+    float rDesiredOutput = controlInterface.getChassisRInput();
+    float rAbs = rDesiredOutput > 0 ? rDesiredOutput : -rDesiredOutput;
+
+    float denominator = yAbs + xAbs + rAbs;
+    denominator = denominator > 1 ? denominator : 1;
+
+    // Set motors desired output    
+    chassis->setDesiredOutputMotor(CHASSIS_MOTOR_ID_BL, (yDesiredOutput - xDesiredOutput + rDesiredOutput) / denominator);
+    chassis->setDesiredOutputMotor(CHASSIS_MOTOR_ID_BR, (yDesiredOutput + xDesiredOutput + rDesiredOutput) / denominator);
+    chassis->setDesiredOutputMotor(CHASSIS_MOTOR_ID_FL, (yDesiredOutput - xDesiredOutput - rDesiredOutput) / denominator);
+    chassis->setDesiredOutputMotor(CHASSIS_MOTOR_ID_FR, (yDesiredOutput + xDesiredOutput - rDesiredOutput) / denominator);
 
     chassis->refresh();
 }
