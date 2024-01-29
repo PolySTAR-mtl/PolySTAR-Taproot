ddbecc1720e0557d58f303b3be7706457a3c21c6
Juliette <juliette.legault@polymtl.ca>
2024-01-28 22:02:00 -0500

    Remove padding from position message

--- PolySTAR-Taproot-project/src/communication/cv_serial_data.hpp
+++ PolySTAR-Taproot-project/src/communication/cv_serial_data.hpp
@@ -11,7 +11,7 @@ namespace src::communication::cv
 {
 /**
  * Contains enum and struct definitions used in the `CVSerial` and `CVHandler` classes
- * 
+ *
  */
 class CVSerialData
 {
@@ -20,7 +20,8 @@ public:
     {
     public:
         // Outgoing message IDs
-        enum MESSAGE_ID : uint8_t {
+        enum MESSAGE_ID : uint8_t
+        {
             GAME_STATUS_MESSAGE = 0x01,
             EVENT_MESSAGE = 0x02,
             TURRET_FEEDBACK_MESSAGE = 0x03,
@@ -28,7 +29,8 @@ public:
         };
 
         // Message lengths in bytes
-        enum DATA_LEN : uint8_t {
+        enum DATA_LEN : uint8_t
+        {
             DATA_LEN_GAME = 14,
             DATA_LEN_EVENT = 2,
             DATA_LEN_TURRET = 4,
@@ -37,7 +39,8 @@ public:
         };
 
         // Aiming mode IDs
-        enum AIMING_MODE : uint8_t {
+        enum AIMING_MODE : uint8_t
+        {
             MANUAL_AIM,
             AUTO_AIM,
             AUTO_FIRE,
@@ -47,15 +50,22 @@ public:
         // Start of frame byte
         static const uint8_t START_OF_FRAME = 0xFC;
 
-        struct CVMessageHeader {
-            CVMessageHeader(uint8_t sof, uint8_t commandID, uint8_t dataLength) : startOfFrame(sof), commandID(commandID), dataLength(dataLength) {};
-            uint8_t startOfFrame;;
+        struct CVMessageHeader
+        {
+            CVMessageHeader(uint8_t sof, uint8_t commandID, uint8_t dataLength)
+                : startOfFrame(sof),
+                  commandID(commandID),
+                  dataLength(dataLength){};
+            uint8_t startOfFrame;
+            ;
             uint8_t commandID;
             uint8_t dataLength;
         } modm_packed;
 
-        struct GameStatusMessage : CVMessageHeader {
-            GameStatusMessage() : CVMessageHeader(START_OF_FRAME, GAME_STATUS_MESSAGE, DATA_LEN_GAME) {};
+        struct GameStatusMessage : CVMessageHeader
+        {
+            GameStatusMessage()
+                : CVMessageHeader(START_OF_FRAME, GAME_STATUS_MESSAGE, DATA_LEN_GAME){};
             uint16_t robotType;
             uint16_t redStdHP;
             uint16_t redHroHP;
@@ -66,19 +76,24 @@ public:
             uint16_t aimingMode;
         } modm_packed;
 
-        struct EventMessage : CVMessageHeader {
-            EventMessage() : CVMessageHeader(START_OF_FRAME, EVENT_MESSAGE, DATA_LEN_EVENT) {};
+        struct EventMessage : CVMessageHeader
+        {
+            EventMessage() : CVMessageHeader(START_OF_FRAME, EVENT_MESSAGE, DATA_LEN_EVENT){};
             RefSerialData::Rx::GameStage gameStage;
         } modm_packed;
 
-        struct TurretMessage : CVMessageHeader {
-            TurretMessage() : CVMessageHeader(START_OF_FRAME, TURRET_FEEDBACK_MESSAGE, DATA_LEN_TURRET) {};
+        struct TurretMessage : CVMessageHeader
+        {
+            TurretMessage()
+                : CVMessageHeader(START_OF_FRAME, TURRET_FEEDBACK_MESSAGE, DATA_LEN_TURRET){};
             int16_t pitch;
             int16_t yaw;
         } modm_packed;
 
-        struct PositionMessage : CVMessageHeader {
-            PositionMessage() : CVMessageHeader(START_OF_FRAME, POSITION_FEEDBACK_MESSAGE, DATA_LEN_POSITION) {};
+        struct PositionMessage : CVMessageHeader
+        {
+            PositionMessage()
+                : CVMessageHeader(START_OF_FRAME, POSITION_FEEDBACK_MESSAGE, DATA_LEN_POSITION){};
             int16_t Ax;
             int16_t Ay;
             int16_t Az;
@@ -89,58 +104,62 @@ public:
             int16_t Ry;
             int16_t Rz;
             uint16_t frontLeftEncoder;
-            int16_t  frontLeftRevolutions;
+            int16_t frontLeftRevolutions;
             uint16_t frontRightEncoder;
-            int16_t  frontRightRevolutions;
+            int16_t frontRightRevolutions;
             uint16_t backLeftEncoder;
-            int16_t  backLeftRevolutions;
+            int16_t backLeftRevolutions;
             uint16_t backRightEncoder;
-            int16_t  backRightRevolutions;
-            int16_t  frontLeftRPM;
-            int16_t  frontRightRPM;
-            int16_t  backLeftRPM;
-            int16_t  backRightRPM;
-            int8_t padding;
+            int16_t backRightRevolutions;
+            int16_t frontLeftRPM;
+            int16_t frontRightRPM;
+            int16_t backLeftRPM;
+            int16_t backRightRPM;
         } modm_packed;
 
     };  // Class Tx
 
-    class Rx 
+    class Rx
     {
     public:
         // Incoming message IDs
-        enum MESSAGE_ID : uint8_t {
+        enum MESSAGE_ID : uint8_t
+        {
             TURRET_MESSAGE = 0x10,
             MOVEMENT_MESSAGE = 0x11,
             SHOOT_MESSAGE = 0x12
         };
 
         /*
-        * Turret pitch and yaw setpoints as received from CV
-        * Angles given in millirad in the local body frame (Origin is barrel straight ahead, parallel to ground)
-        */
-        struct TurretData {
+         * Turret pitch and yaw setpoints as received from CV
+         * Angles given in millirad in the local body frame (Origin is barrel straight ahead,
+         * parallel to ground)
+         */
+        struct TurretData
+        {
             int16_t pitchSetpoint;
             int16_t yawSetpoint;
         };
 
         /*
-        * Chassis movement setpoints as received from CV
-        * x, y in mm/s
-        * r in millirad/s
-        */
-        struct MovementData {
+         * Chassis movement setpoints as received from CV
+         * x, y in mm/s
+         * r in millirad/s
+         */
+        struct MovementData
+        {
             int16_t xSetpoint;
             int16_t ySetpoint;
             int16_t rSetpoint;
         };
 
-        struct ShootOrderData {
+        struct ShootOrderData
+        {
             uint8_t shootOrder;
         };
 
     };  // Class Rx
-}; // Class CVSerialData
+};      // Class CVSerialData
 }  // Namespace src::communication::cv
 
-#endif // CV_SERIAL_DATA_HPP_
+#endif  // CV_SERIAL_DATA_HPP_
