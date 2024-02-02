396e88d6d714f4b9a49196fb2f491e3a7dd7e3d4
Heartpray3 <elycheikh2002@hotmail.com>
2024-02-01 19:34:27 -0500

    ADD calling the generic function from GenericTestAutoAimCommand

--- PolySTAR-Taproot-project/src/subsystems/turret/turret_test_auto_aim_command.cpp
+++ PolySTAR-Taproot-project/src/subsystems/turret/turret_test_auto_aim_command.cpp
@@ -3,8 +3,6 @@
 #include "tap/algorithms/math_user_utils.hpp"
 #include "tap/errors/create_errors.hpp"
 
-using src::communication::cv::CVSerialData;
-
 namespace control
 {
 namespace turret
@@ -18,12 +16,7 @@ TurretTestAutoAimCommand::TurretTestAutoAimCommand(
 
 void  TurretTestAutoAimCommand::execute()
 {
-    // Acquire setpoints received from CV over serial through CVHandler
-    CVSerialData::Rx::TurretData turretData = drivers->cvHandler.getTurretData();
-    float pitchSetpoint = turretData.pitchSetpoint*MRAD_TO_DEGREES;
-    float yawSetpoint = turretData.yawSetpoint*MRAD_TO_DEGREES;
-
-    turret->setAbsoluteOutputDegrees(yawSetpoint, pitchSetpoint);
+    GenericAutoAimCommand::execute();
 }
 }  // namespace turret
 }  // namespace control
