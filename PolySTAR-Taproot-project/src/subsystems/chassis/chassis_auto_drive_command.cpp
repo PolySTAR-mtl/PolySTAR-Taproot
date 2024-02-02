251c8e5f5bdc0e519b2b3a729c247fbb7158ebfa
Heartpray3 <elycheikh2002@hotmail.com>
2024-02-01 19:25:00 -0500

    ADD inheriting generic auto drive command to chassis auto drive command, where the code is based in the generic class

--- PolySTAR-Taproot-project/src/subsystems/chassis/chassis_auto_drive_command.cpp
+++ PolySTAR-Taproot-project/src/subsystems/chassis/chassis_auto_drive_command.cpp
@@ -1,8 +1,5 @@
 #include "chassis_auto_drive_command.hpp"
 
-#include "tap/algorithms/math_user_utils.hpp"
-#include "tap/errors/create_errors.hpp"
-
 using src::communication::cv::CVSerialData;
 
 namespace control
@@ -12,17 +9,9 @@ namespace chassis
 ChassisAutoDriveCommand::ChassisAutoDriveCommand(
     ChassisSubsystem *const chassis,
     src::Drivers *drivers)
-    : chassis(chassis),
-      drivers(drivers)
+    : GenericAutoDriveCommand(chassis, drivers)
 {
-    if (chassis == nullptr)
-    {
-        return;
-    }
-    this->addSubsystemRequirement(dynamic_cast<tap::control::Subsystem *>(chassis));
 }
-
-void  ChassisAutoDriveCommand::initialize() {}
 
 void  ChassisAutoDriveCommand::execute()
 {
@@ -31,20 +20,9 @@ void  ChassisAutoDriveCommand::execute()
         chassis->setTargetOutput(0, 0, 0);
         return;
     }
-    // Acquire setpoints received from CV over serial through CVHandler
-    // And convert velocities to chassis inputs
-    CVSerialData::Rx::MovementData movementData = drivers->cvHandler.getMovementData();
-    float x = movementData.xSetpoint*VX_TO_X;
-    float y = movementData.ySetpoint*VY_TO_Y;
-    float r = movementData.rSetpoint*W_TO_R;
-    chassis->setTargetOutput(x,y,r);
+    GenericAutoDriveCommand::execute();
 }
 
-void  ChassisAutoDriveCommand::end(bool) {
-    chassis->setTargetOutput(0,0,0);
-}
-
-bool  ChassisAutoDriveCommand::isFinished() const { return false; }
 }  // namespace chassis
 }  // namespace control
 
