740883e0f73e34890bfc4cae3ecf78c3d0a33db2
Heartpray3 <elycheikh2002@hotmail.com>
2024-02-01 19:21:18 -0500

    ADD generic auto command for turret auto aim and turret test auto aim, so it would not have duplicated code

---
+++ PolySTAR-Taproot-project/src/subsystems/turret/generic_auto_aim_command.hpp
@@ -0,0 +1,51 @@
+#ifndef GENERIC_AUTO_AIM_COMMAND_HPP_
+#define GENERIC_AUTO_AIM_COMMAND_HPP_
+
+#include "tap/control/command.hpp"
+
+#include "turret_subsystem.hpp"
+#include "control/drivers/drivers.hpp"
+
+namespace control
+{
+namespace turret
+{
+class GenericAutoAimCommand : public tap::control::Command
+{
+public:
+    /**
+     * Initializes the command with the passed in ChassisSubsystem.  Must not
+     * be nullptr.
+     *
+     * @param[in] turret a pointer to the chassis to be passed in that this
+     *      Command will interact with.
+     */
+    GenericAutoAimCommand(TurretSubsystem *const turret, src::Drivers *drivers);
+
+    GenericAutoAimCommand(const GenericAutoAimCommand &other) = delete;
+
+    GenericAutoAimCommand &operator=(const GenericAutoAimCommand &other) = delete;
+
+    void initialize() override;
+
+    virtual void execute() override;
+
+    void end(bool) override;
+
+    bool isFinished() const override;
+
+protected:
+    TurretSubsystem *const turret;
+
+    src::Drivers *drivers;
+
+    const float MRAD_TO_DEGREES = 0.0572958;
+
+};  // GenericAutoAimCommand
+
+}  // namespace turret
+
+}  // namespace control
+
+#endif  // GENERIC_AUTO_AIM_COMMAND_HPP_
+
