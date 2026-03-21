79bd0aecfcac8ebe9b7436d37b61d628f3b52662
Glory <tsarra-glory.randrianandraina@polymtl.ca>
2026-03-20 21:06:51 -0400

    feat: polymorphism implemented and tested

--- PolySTAR-Taproot-project/src/subsystems/chassis/algorithms/chassis_gains.hpp
+++ PolySTAR-Taproot-project/src/subsystems/chassis/algorithms/chassis_gains.hpp
@@ -7,3 +7,38 @@ namespace control::chassis::algorithms
         float k_vel;
     };
 }
+
+#ifdef TARGET_HERO
+// defauilt lqr gains (standard)
+static constexpr control::chassis::algorithms::Gains2 Kx = {63.24f, 11.25f}; 
+static constexpr control::chassis::algorithms::Gains2 Ky = {63.24f, 11.25f};
+static constexpr control::chassis::algorithms::Gains2 Kt = {15.81f, 2.45f};
+#endif
+
+#ifdef TARGET_SENTRY
+// defauilt lqr gains (standard)
+static constexpr control::chassis::algorithms::Gains2 Kx = {63.24f, 11.25f}; 
+static constexpr control::chassis::algorithms::Gains2 Ky = {63.24f, 11.25f};
+static constexpr control::chassis::algorithms::Gains2 Kt = {15.81f, 2.45f};
+#endif
+
+#ifdef TARGET_STANDARD
+// defauilt lqr gains (standard)
+static constexpr control::chassis::algorithms::Gains2 Kx = {63.24f, 11.25f}; 
+static constexpr control::chassis::algorithms::Gains2 Ky = {63.24f, 11.25f};
+static constexpr control::chassis::algorithms::Gains2 Kt = {15.81f, 2.45f};
+#endif
+
+#ifdef TARGET_ICRA
+// defauilt lqr gains (standard)
+static constexpr control::chassis::algorithms::Gains2 Kx = {63.24f, 11.25f}; 
+static constexpr control::chassis::algorithms::Gains2 Ky = {63.24f, 11.25f};
+static constexpr control::chassis::algorithms::Gains2 Kt = {15.81f, 2.45f};
+#endif
+
+#ifdef TARGET_SPIN_TO_WIN
+// defauilt lqr gains (standard)
+static constexpr control::chassis::algorithms::Gains2 Kx = {63.24f, 11.25f}; 
+static constexpr control::chassis::algorithms::Gains2 Ky = {63.24f, 11.25f};
+static constexpr control::chassis::algorithms::Gains2 Kt = {15.81f, 2.45f};
+#endif
