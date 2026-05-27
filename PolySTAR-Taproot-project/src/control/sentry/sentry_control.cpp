ce06267f67dbce1661e311c4bb48d638f25894f0
Hien-Minh Tu <tuhienminh0@gmail.com>
2026-05-26 21:01:17 -0400

    Fix imports in sentry control

--- PolySTAR-Taproot-project/src/control/sentry/sentry_control.cpp
+++ PolySTAR-Taproot-project/src/control/sentry/sentry_control.cpp
@@ -25,16 +25,16 @@
 #include "subsystems/turret/turret_test_auto_aim_command.hpp"
 
 // Feeder includes
-#include "subsystems/feeder/feeder_velocity_subsystem.hpp"
-#include "subsystems/feeder/feeder_auto_feed_command.hpp"
-#include "subsystems/feeder/feeder_auto_feed_test_command.hpp"
+#include "subsystems/feeder/core/feeder_velocity_subsystem.hpp"
+#include "subsystems/feeder/commands/feeder_auto_feed_command.hpp"
+#include "subsystems/feeder/commands/feeder_auto_feed_test_command.hpp"
 
 #include "subsystems/feeder/core/feeder_position_subsystem.hpp" // Test with controller
 #include "subsystems/feeder/commands/feeder_move_unjam_command.hpp"
 #include "subsystems/feeder/commands/feeder_move_command.hpp"
 
 // Flywheel includes
-#include "subsystems/flywheel/core/flywheel_fire_commands.hpp"
+#include "subsystems/flywheel/commands/flywheel_fire_commands.hpp"
 #include "subsystems/flywheel/core/flywheel_dji_subsystem.hpp"
 
 // Motor includes
