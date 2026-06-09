338bd1793925025a136fb94cdc045b45eb1ab03e
AbdoulhakimAI <abdoulhakim.abdillahi@gmail.com>
2026-06-09 19:03:49 -0400

    feat: added the skeleton of the templated refactor

--- PolySTAR-Taproot-project/src/subsystems/chassis/modes/chassis_operation_mode.cpp
+++ PolySTAR-Taproot-project/src/subsystems/chassis/modes/chassis_operation_mode.cpp
@@ -45,40 +45,7 @@ namespace control::chassis
 
     void ChassisOperationMode::manualMode(ChassisHeroCommand* command)
     {
-        if (command == nullptr) {
-            return;
-        }
-        
-        auto& drivers = command->drivers;
-        auto& turretYawMotor = command->turretYawMotor;
-        auto& m_isMoving = command->m_isMoving;
-        auto& chassis = command->chassis;
-
-        float xInput = drivers->controlInterface.getChassisXInput();
-        float yInput = drivers->controlInterface.getChassisYInput();
-
-        m_isMoving = sqrt(xInput*xInput+yInput*yInput) > CHASSIS_DEAD_ZONE;
-
-        // float rotationAngle = turretYawMotor->getEncoderUnwrapped();
-        // chassis->setRotationAngle(rotationAngle);
-
-        // Chassis joystick orientation in radians
-        float chassisRad = atan2(yInput, xInput);
-
-        // Turret yaw orientation 
-        int64_t yawDelta = turretYawMotor->getEncoderWrapped() - YAW_NEUTRAL_POS;
-        float yawDeltaRad = tap::motor::DjiMotor::encoderToDegrees<int64_t>(yawDelta) * std::numbers::pi / 180;
-
-        float d = sqrt(pow(xInput, 2) + pow(yInput, 2));
-        float x = d * cos(chassisRad + yawDeltaRad);
-        float y = d * sin(chassisRad + yawDeltaRad);
-
-        float r = m_isMoving ? ROTATION_SPEED_LOW : ROTATION_SPEED_HIGH;
-
-        chassis->setTargetOutput(
-            fabs(x) >= CHASSIS_DEAD_ZONE ? x : 0.0f,
-            fabs(y) >= CHASSIS_DEAD_ZONE ? y : 0.0f,
-            fabs(r) >= CHASSIS_DEAD_ZONE ? r : 0.0f);
+        return;
     }
 
     void ChassisOperationMode::autoMode(ChassisSentryCommand* command) 
@@ -104,4 +71,29 @@ namespace control::chassis
         const float r = movementData.rSetpoint*W_TO_R;
         chassis->setTargetOutput(x,y,r);
     }
+
+    ChassisInputs ChassisOperationMode::getChassisInputs(ChassisSentryCommand* command)
+    {
+        return;
+    }
+
+    template<typename Command>
+    ChassisInputs ChassisOperationMode::getChassisInputs<Command>(Command *const command)
+    {
+        return;
+    }
+
+    template<typename Command>
+    SubsystemCoords ChassisOperationMode::calculateSubsystemCoords<Command>(Command *const command, const ChassisInputs& inputs)
+    {
+        return;
+    }
+
+    template<typename Command>
+    void ChassisOperationMode::setOuput<Command>(Command *const command, const SubsystemCoords& coords)
+    {
+        return;
+    }
+
+
 } // namespace control::chassis
--- PolySTAR-Taproot-project/src/subsystems/chassis/modes/chassis_operation_mode.hpp
+++ PolySTAR-Taproot-project/src/subsystems/chassis/modes/chassis_operation_mode.hpp
@@ -9,12 +9,46 @@
 namespace control::chassis
 {
 
-struct ChassisOperationMode 
+enum class OperationType
+{
+    Auto = 0,
+    Manual,
+    None,
+};
+
+struct ChassisInputs 
+{
+    float x = 0.0f;
+    float y = 0.0f;
+    float r = 0.0f;
+    OperationType operationType = OperationType::None;
+};
+
+struct SubsystemCoords 
+{
+    float x = 0.0f;
+    float y = 0.0f;
+    float r = 0.0f;
+};
+
+struct ChassisOperationMode
 {
     static void manualMode(ChassisSpin2winCommand* command);
     static void manualMode(ChassisHeroCommand* command);
     static void autoMode(ChassisSentryCommand* command);
-}
+
+    static ChassisInputs getChassisInputs(ChassisSentryCommand* command);
+
+    template<typename Command>
+    static ChassisInputs getChassisInputs(Command *const command);
+
+    template<typename Command>
+    static SubsystemCoords calculateSubsystemCoords(Command *const command, const ChassisInputs& inputs);
+
+    template<typename Command>
+    static void setOuput(Command* command, const SubsystemCoords& coords);
+};
+
 
 } // namespace control::chassis
 
