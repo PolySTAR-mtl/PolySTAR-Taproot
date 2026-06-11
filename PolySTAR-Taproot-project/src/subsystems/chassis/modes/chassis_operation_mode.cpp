82b5185929a04acf887997833edfed9232ddf18a
AbdoulhakimAI <abdoulhakim.abdillahi@gmail.com>
2026-06-10 22:33:07 -0400

    feat: added the full implementation with templates

--- PolySTAR-Taproot-project/src/subsystems/chassis/modes/chassis_operation_mode.cpp
+++ PolySTAR-Taproot-project/src/subsystems/chassis/modes/chassis_operation_mode.cpp
@@ -10,42 +10,22 @@ namespace control::chassis
         if (command == nullptr) {
             return;
         }
-        
-        auto& drivers = command->drivers;
-        auto& turretYawMotor = command->turretYawMotor;
-        auto& m_isMoving = command->m_isMoving;
-        auto& chassis = command->chassis;
 
-        float xInput = drivers->controlInterface.getChassisXInput();
-        float yInput = drivers->controlInterface.getChassisYInput();
-
-        m_isMoving = sqrt(xInput*xInput+yInput*yInput) > CHASSIS_DEAD_ZONE;
-
-        // float rotationAngle = turretYawMotor->getEncoderUnwrapped();
-        // chassis->setRotationAngle(rotationAngle);
-
-        // Chassis joystick orientation in radians
-        float chassisRad = atan2(yInput, xInput);
-
-        // Turret yaw orientation 
-        int64_t yawDelta = turretYawMotor->getEncoderWrapped() - YAW_NEUTRAL_POS;
-        float yawDeltaRad = tap::motor::DjiMotor::encoderToDegrees<int64_t>(yawDelta) * std::numbers::pi / 180;
-
-        float d = sqrt(pow(xInput, 2) + pow(yInput, 2));
-        float x = d * cos(chassisRad + yawDeltaRad);
-        float y = d * sin(chassisRad + yawDeltaRad);
-
-        float r = m_isMoving ? ROTATION_SPEED_LOW : ROTATION_SPEED_HIGH;
-
-        chassis->setTargetOutput(
-            fabs(x) >= CHASSIS_DEAD_ZONE ? x : 0.0f,
-            fabs(y) >= CHASSIS_DEAD_ZONE ? y : 0.0f,
-            fabs(r) >= CHASSIS_DEAD_ZONE ? r : 0.0f);
+        ChassisInputs inputs = getChassisInputs(command);
+        SubsystemCoords coords = calculateSubsystemCoords(command, inputs);
+        setOuput(command, coords);
     }
 
     void ChassisOperationMode::manualMode(ChassisHeroCommand* command)
     {
-        return;
+        if (command == nullptr) 
+        {
+            return;
+        }
+
+        ChassisInputs inputs = getChassisInputs(command);
+        SubsystemCoords coords = calculateSubsystemCoords(command, inputs);
+        setOuput(command, coords);
     }
 
     void ChassisOperationMode::autoMode(ChassisSentryCommand* command) 
@@ -54,45 +34,86 @@ namespace control::chassis
             return;
         }
 
-        auto& drivers = command->drivers;
-        auto& chassis = command->chassis;
-        auto& matchTimeout = command->startMatchTimeout;
-
-        if (!matchTimeout.isExpired())
-        {
-            chassis->setTargetOutput(0, 0, 0);
-            return;
-        }
-        drivers->leds.set(tap::gpio::Leds::A, true);
-
-        const auto& movementData = drivers->cvHandler.getMovementData();
-        const float x = movementData.xSetpoint*VX_TO_X;
-        const float y = movementData.ySetpoint*VY_TO_Y;
-        const float r = movementData.rSetpoint*W_TO_R;
-        chassis->setTargetOutput(x,y,r);
+        ChassisInputs inputs = getChassisInputs(command);
+        SubsystemCoords coords = calculateSubsystemCoords(command, inputs);
+        setOuput(command, coords);    
     }
 
     ChassisInputs ChassisOperationMode::getChassisInputs(ChassisSentryCommand* command)
     {
-        return;
+        if (!command->startMatchTimeout.isExpired()) 
+        {
+            return { 0.0f, 0.0f, 0.0f, OperationType::Auto };
+        }
+
+        command->drivers->leds.set(tap::gpio::Leds::A, true);
+        const auto& movementData = command->drivers->cvHandler.getMovementData();
+        return { 
+            movementData.xSetpoint, 
+            movementData.ySetpoint, 
+            movementData.rSetpoint, 
+            OperationType::Auto 
+        };
     }
 
     template<typename Command>
     ChassisInputs ChassisOperationMode::getChassisInputs<Command>(Command *const command)
     {
-        return;
+        return {
+            command->drivers->controlInterface.getChassisXInput(),
+            command->drivers->controlInterface.getChassisYInput(),
+            0.0f,
+            OperationType::Manual,
+        };
     }
 
     template<typename Command>
     SubsystemCoords ChassisOperationMode::calculateSubsystemCoords<Command>(Command *const command, const ChassisInputs& inputs)
     {
-        return;
+        if (inputs.operationType == OperationType::Auto) 
+        {
+            return 
+            { 
+                inputs.x * VX_TO_X, 
+                inputs.y * VY_TO_Y, 
+                inputs.r * W_TO_R
+            };
+        }
+
+        command->m_isMoving = sqrt(xInput*xInput+yInput*yInput) > CHASSIS_DEAD_ZONE;
+
+        // const float rotationAngle = command->turretYawMotor->getEncoderUnwrapped();
+        // command->chassis->setRotationAngle(rotationAngle);
+
+        // Chassis joystick orientation in radians
+        const float chassisRad = atan2(inputs.x, inputs.y);
+
+        // Turret yaw orientation 
+        int64_t yawDelta = command->turretYawMotor->getEncoderWrapped() - YAW_NEUTRAL_POS;
+        float yawDeltaRad = tap::motor::DjiMotor::encoderToDegrees<int64_t>(yawDelta) * std::numbers::pi / 180;
+
+        const float d = sqrt(pow(xInput, 2) + pow(yInput, 2));
+        const float x = d * cos(chassisRad + yawDeltaRad);
+        const float y = d * sin(chassisRad + yawDeltaRad);
+
+        float r = m_isMoving ? ROTATION_SPEED_LOW : ROTATION_SPEED_HIGH;
+        
+        return { 
+            fabs(x) >= CHASSIS_DEAD_ZONE ? x : 0.0f,
+            fabs(y) >= CHASSIS_DEAD_ZONE ? y : 0.0f,
+            fabs(r) >= CHASSIS_DEAD_ZONE ? r : 0.0f
+        };
     }
 
     template<typename Command>
     void ChassisOperationMode::setOuput<Command>(Command *const command, const SubsystemCoords& coords)
     {
-        return;
+        if (command == nullptr) 
+        {
+            return;
+        }
+
+        command->chassis->setTargetOutput(coords.x, coord.y, coords.r);
     }
 
 
